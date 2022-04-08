af28be043acd7e856973682c9ce6f8623446e459
priyam <priyamshankar.5@gmail.com>
2022-04-08 21:13:01 +0530

    data types and input

---
+++ dataTypesInPython.py
@@ -0,0 +1,5 @@
+name="priyam"
+age=20
+cgpa=8.2
+anyBack=False
+print(name,age,cgpa,anyBack)
---
+++ takingtheInput.py
@@ -0,0 +1,8 @@
+name="priyam"
+age=20
+cgpa=8.2
+anyBack=False
+print(name,age,cgpa,anyBack)
+
+name=input("what is your name?")
+print(name)
